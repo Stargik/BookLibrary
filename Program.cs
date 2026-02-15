ac065a0c12fb35d1d159a81445e8c581c026b923
aleksandrstarzhynskyi <a.stargik@gmail.com>
2026-02-15 23:23:39 +0200

    Added migration

--- Program.cs
+++ Program.cs
@@ -14,6 +14,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+// Apply database migrations automatically
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
